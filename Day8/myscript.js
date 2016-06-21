242d74b284f83e7eb6629caea414e540fbc85802
Nitin Deepak <nitin.d12@gmail.com>
2016-06-21 12:02:22 +0530

    updates
    updates

---
+++ Day8/myscript.js
@@ -0,0 +1,16 @@
+function validateForm(){
+		if(document.frmLogin.txtUsr.value == ''){
+			document.getElementById('msg_').innerHTML = "X: Username please!";
+			document.frmLogin.txtUsr.focus();
+			document.frmLogin.txtUsr.style.background = "#ffff00";
+		} else if(document.frmLogin.txtPwd.value == '') {
+			document.getElementById('msg_').innerHTML = "X: Password please!"
+			document.frmLogin.txtPwd.focus();
+			document.frmLogin.txtPwd.style.background = "#ffff00";
+		} else {
+			document.frmLogin.txtUsr.style.background = "#ffffff";
+			document.frmLogin.txtPwd.style.background = "#ffffff";
+			document.getElementById('msg_').innerHTML = "";
+			//document.frmLogin.submit();
+		}
+}
